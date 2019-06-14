c3b69039ccfe2e7da9072b6199eb4ac482799239
mspark109 <minsup109@gmail.com>
2019-06-15 00:28:38 +0900

    2차원배열 추가

---
+++ Notepad/src/main/ArrayExam.java
@@ -0,0 +1,29 @@
+package main;
+
+public class ArrayExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] array1 = new int [2][3];
+		
+		/*
+		 * array1
+		 * [0] - {[],[],[]}
+		 * [1] - {[],[],[]}
+		 */
+		
+		int[][] array2 = new int[3][];
+		array2[2] = new int[3];
+		
+		array2[2][2] = 10;
+		/*
+		 * array2
+		 * [0] - {}
+		 * [1] - {}
+		 * [2] - {[],[],[10]}
+		 */
+		
+		System.out.println(array2[2][2]);
+	}
+
+}
