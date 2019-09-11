4eefca3405f2facc5e915623138a54262c33b7ee
hns <hns@172.16.216.41>
2019-09-11 16:52:28 +0900

    난수구하기 Math.random()추가

--- Notepad/src/main/Ex09_RandomNumber.java
+++ Notepad/src/main/Ex09_RandomNumber.java
@@ -9,12 +9,18 @@ public class Ex09_RandomNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// 0~9 난수 구하기
+		System.out.println("==========");
+		for(int j=0; j<10; j++) System.out.println((int)(Math.random() * 10));
+		
 		Random rn1 = new Random();
 		for(int i=0; i<10; i++) System.out.println(rn1.nextInt(100));
 		
 		System.out.println("==========");
 		Random rn2 = new java.security.SecureRandom();
 		for(int j=0; j<10; j++) System.out.println(rn2.nextInt(100));
+		
+		
 		
 		
 		/*String encryptStr = "";
